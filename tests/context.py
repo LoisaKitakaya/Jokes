0f55a360285e15b27ecd45e8909d562e5a0be57a
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-07 10:44:04 +0300

    shorter more simplified context

--- tests/context.py
+++ tests/context.py
@@ -1,51 +1,29 @@
 from dad_jokes.jokes import DadJokes
 
-# jokes module
+# jokes module: DadJokes class
+
+dad_jokes = DadJokes()
 
 
 def random_json():
-    res = DadJokes()
-
-    response = res.random_joke_as_json()
-
-    return response
+    return dad_jokes.random_joke_as_json()
 
 
 def random_text():
-    res = DadJokes()
-
-    response = res.random_joke_as_text()
-
-    return response
+    return dad_jokes.random_joke_as_text()
 
 
 def specific_json(id):
-    res = DadJokes()
-
-    response = res.specific_joke_as_json(id=id)
-
-    return response
+    return dad_jokes.specific_joke_as_json(id=id)
 
 
 def specific_text(id):
-    res = DadJokes()
-
-    response = res.specific_joke_as_text(id=id)
-
-    return response
+    return dad_jokes.specific_joke_as_text(id=id)
 
 
 def list_json(page, limit, term):
-    res = DadJokes()
-
-    response = res.list_jokes_as_json(page=page, limit=limit, term=term)
-
-    return response
+    return dad_jokes.list_jokes_as_json(page=page, limit=limit, term=term)
 
 
 def list_text(limit):
-    res = DadJokes()
-
-    response = res.list_jokes_as_text(limit=limit)
-
-    return response
+    return dad_jokes.list_jokes_as_text(limit=limit)
